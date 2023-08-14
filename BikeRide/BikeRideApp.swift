a6bb63917c00dd4faecc65244ef7e9686f21b629
jackoehling <oehling.j@northeastern.edu>
2023-08-13 20:07:54 -0400

    Initial Commit

---
+++ BikeRide/BikeRideApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeRideApp.swift
+//  BikeRide
+//
+//  Created by Jack Oehling on 8/13/23.
+//
+
+import SwiftUI
+
+@main
+struct BikeRideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
